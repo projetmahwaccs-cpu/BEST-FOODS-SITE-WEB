17bd3dd7b107bcb41f80ef78e77301b855fea0b1
projetmahwaccs-cpu <projetmahwaccs@gmail.com>
2026-05-09 17:23:43 -0700

    Add navigation toggle functionality to script.js

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
